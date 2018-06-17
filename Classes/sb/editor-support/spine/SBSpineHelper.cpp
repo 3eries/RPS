c03e030c440d72196bcd5dbfb4484e2a3a75a11d
seongmin hwang <>
2018-06-17 13:42:10 +0900

    [추가] 자동 삭제 기능

--- Classes/sb/editor-support/spine/SBSpineHelper.cpp
+++ Classes/sb/editor-support/spine/SBSpineHelper.cpp
@@ -13,33 +13,44 @@ using namespace spine;
 using namespace std;
 
 SkeletonAnimation* SBSpineHelper::runAnimation(SBCallback completeListener,
-                                            const string &jsonFile, const string &animName) {
+                                               const string &jsonFile, const string &animName,
+                                               bool autoRemove) {
     
     auto anim = SkeletonAnimation::createWithJsonFile(jsonFile);
     anim->setAnchorPoint(Vec2::ZERO);
     anim->setPosition(Vec2(SB_WIN_SIZE*0.5f));
     
     if( animName != "" ) {
-        runAnimation(completeListener, anim, animName);
+        runAnimation(completeListener, anim, animName, autoRemove);
     }
     
     return anim;
 }
 
-void SBSpineHelper::runAnimation(SBCallback completeListener, SpineAnimation *anim, const string &animName) {
+void SBSpineHelper::runAnimation(SBCallback completeListener,
+                                 SpineAnimation *anim, const string &animName,
+                                 bool autoRemove) {
     
     CCASSERT(anim != nullptr, "SBSpineHelper::runAnimation error: invalid anim.");
     CCASSERT(animName != "", "SBSpineHelper::runAnimation error: invalid anim name.");
     
     auto track = anim->setAnimation(0, animName, false);
     
-    if( !completeListener ) {
-        return;
-    }
-    
     anim->setTrackCompleteListener(track, [=](spTrackEntry *entry) {
         
         SB_SAFE_PERFORM_LISTENER(anim, completeListener);
+        
+        if( !autoRemove ) {
+            return;
+        }
+        
+        // remove
+        anim->clearTracks();
+        anim->setVisible(false);
+        
+        auto delay = DelayTime::create(0.1f);
+        auto remove = RemoveSelf::create();
+        anim->runAction(Sequence::create(delay, remove, nullptr));
     });
 }
 
--- Classes/sb/editor-support/spine/SBSpineHelper.hpp
+++ Classes/sb/editor-support/spine/SBSpineHelper.hpp
@@ -19,10 +19,12 @@ public:
     
 public:
     static SpineAnimation* runAnimation(SBCallback completeListener,
-                                        const std::string &jsonFile, const std::string &animName = "");
+                                        const std::string &jsonFile, const std::string &animName = "",
+                                        bool autoRemove = false);
     
     static void runAnimation(SBCallback completeListener,
-                             SpineAnimation *anim, const std::string &animName);
+                             SpineAnimation *anim, const std::string &animName,
+                             bool autoRemove = false);
     static void clearAnimation(SpineAnimation *anim, std::string clearAnimName = "");
 };
 
