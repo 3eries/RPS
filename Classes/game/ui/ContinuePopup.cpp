94fc9652007d670f33b3231fef694ee6e35c4afa
seongmin hwang <>
2018-07-07 22:43:51 +0900

    [added] Continue 팝업 디자인 적용 1차

--- Classes/game/ui/ContinuePopup.cpp
+++ Classes/game/ui/ContinuePopup.cpp
@@ -17,11 +17,18 @@ using namespace std;
 static const string SCHEDULER_COUNTDOWN = "SCHEDULER_COUNTDOWN";
 static const int    COUNTDOWN           = 10;
 
-ContinuePopup::ContinuePopup() : SBBasePopup(),
-onClosedListener(nullptr),
+static const float ENTER_DURATION       = 0.2f;
+static const float EXIT_DURATION        = 0.2f;
+
+#define COUNTDOWN_POS_LEFT              Vec2MC(-800, 100)
+#define COUNTDOWN_POS_CENTER            Vec2MC(0, 100)
+#define COUNTDOWN_POS_RIGHT             Vec2MC(800, 100)
+
+ContinuePopup::ContinuePopup() : BasePopup(Type::CONTINUE),
 onVideoListener(nullptr),
 onTimeOutListener(nullptr),
-elapsed(0) {
+elapsed(0),
+count(COUNTDOWN) {
     
 }
 
@@ -31,37 +38,20 @@ ContinuePopup::~ContinuePopup() {
 
 bool ContinuePopup::init() {
     
-    if( !SBBasePopup::init() ) {
+    if( !BasePopup::init() ) {
         return false;
     }
     
-    initBg();
-    initMenu();
-    
-    // 연출
-    contentLayer->setPositionY(SB_WIN_SIZE.height);
-    
-    auto move = MoveTo::create(0.3f, Vec2(contentLayer->getPositionX(), 0));
-    auto callFunc = CallFunc::create([=]() {
-        this->countdown();
-    });
-    contentLayer->runAction(Sequence::create(move, callFunc, nullptr));
+    runEnterAction();
     
     return true;
 }
 
 void ContinuePopup::onExit() {
     
-    SBBasePopup::onExit();
-}
-
-/**
- * 팝업 제거
- */
-void ContinuePopup::close() {
+    getScheduler()->setTimeScale(1);
     
-    onClosedListener();
-    removeFromParent();
+    BasePopup::onExit();
 }
 
 /**
@@ -78,7 +68,9 @@ void ContinuePopup::countdown() {
             i = MAX(0, i);
             i = MIN(COUNTDOWN-1, i);
             
-            countdownLabel->setString(TO_STRING(i));
+            count = i;
+            this->updateCountdownImage();
+//            countdownLabel->setString(TO_STRING(i));
         }
     };
     
@@ -114,60 +106,133 @@ void ContinuePopup::timeOut() {
     onTimeOutListener();
     
     unscheduleAllCallbacks();
-    close();
+    dismiss();
 }
 
-void ContinuePopup::initBg() {
+void ContinuePopup::updateCountdownImage() {
     
-    auto bg = LayerColor::create(Color::POPUP_BG);
-    addChild(bg);
+    string file = DIR_IMG_GAME + STR_FORMAT("RSP_continue_%02d.png", count);
+    countdownImage->setTexture(file);
+}
+
+void ContinuePopup::initBackgroundView() {
     
-    // background fade in
-    {
-        bg->setOpacity(0);
-        bg->runAction(FadeTo::create(0.2f, 255*0.3f));
-    }
+    BasePopup::initBackgroundView();
     
-    contentLayer = SBNodeUtils::createZeroSizeNode();
-    addChild(contentLayer);
+    // setBackgroundColor(Color::POPUP_BG);
+    setBackgroundColor(Color4B(0,0,0,255*0.5f));
+}
+
+void ContinuePopup::initContentView() {
+    
+    BasePopup::initContentView();
     
     // 타이틀
-    auto titleLabel = Label::createWithTTF("Continue?", FONT_RETRO, 95);
-    titleLabel->setAnchorPoint(ANCHOR_M);
-    titleLabel->setPosition(Vec2MC(0, 350));
-    titleLabel->setColor(Color3B::WHITE);
-//    titleLabel->enableOutline(Color4B::BLACK, 3);
-    titleLabel->enableShadow(Color4B(138,125,164,255), Size(4,-4), 10);
-    contentLayer->addChild(titleLabel);
+    // text_continue.png Vec2TC(2, -222) , Size(580, 84)
+    auto title = Sprite::create(DIR_IMG_GAME + "text_continue.png");
+    title->setTag(Tag::IMG_TITLE);
+    title->setAnchorPoint(ANCHOR_M);
+    title->setPosition(Vec2TC(0, -222));
+    addChild(title);
     
     // 카운트 다운
+    // RSP_continue_10.png Vec2MC(0, 100) , Size(568, 416)
+    countdownImage = Sprite::create(DIR_IMG_GAME + "RSP_continue_10.png");
+    countdownImage->setAnchorPoint(ANCHOR_M);
+    countdownImage->setPosition(COUNTDOWN_POS_CENTER);
+    addChild(countdownImage);
+    
+    /*
     countdownLabel = Label::createWithTTF("", FONT_RETRO, 300);
     countdownLabel->setAnchorPoint(ANCHOR_M);
     countdownLabel->setPosition(Vec2MC(0, 100));
     countdownLabel->setColor(Color3B::WHITE);
     countdownLabel->enableShadow(Color4B(138,125,164,255), Size(4,-4), 10);
-    contentLayer->addChild(countdownLabel);
-}
-
-void ContinuePopup::initMenu() {
+    addChild(countdownLabel);
+    */
     
     // 비디오 버튼
-    auto videoBtn = SBButton::create(SBButton::Config("", Size(500, 80),
-                                                      "VIDEO (INSERT COIN)", FONT_RETRO, 35));
-    videoBtn->setZoomScale(0.1f);
+    // btn_continue.png Vec2BC(0, 392) , Size(520, 152)
+    auto videoBtn = SBButton::create(DIR_IMG_GAME + "btn_continue.png");
+    videoBtn->setTag(Tag::BTN_VIDEO);
+    videoBtn->setZoomScale(0.05f);
     videoBtn->setAnchorPoint(ANCHOR_M);
-    videoBtn->setPosition(Vec2BC(0, 370));
-    addChild(videoBtn, SBZOrder::BOTTOM);
-    
-    {
-        auto titleLabel = videoBtn->getTitle();
-        titleLabel->setColor(Color3B::WHITE);
-        titleLabel->enableOutline(Color4B::BLACK, 3);
-    }
+    videoBtn->setPosition(Vec2BC(0, 392));
+    addChild(videoBtn, 1);
     
     videoBtn->setOnClickListener([=](Node*) {
         
         onVideoListener();
-        this->close();
+        this->dismiss();
     });
+    
+}
+
+/**
+ * 등장 연출
+ */
+void ContinuePopup::runEnterAction(SBCallback onFinished) {
+
+    BasePopup::runEnterAction(onFinished);
+    
+    // 배경
+    runBackgroundFadeInAction(nullptr, 0.15f);
+    
+    // 타이틀
+    auto scale1 = ScaleTo::create(0.15f, 1.1f);
+    auto scale2 = ScaleTo::create(0.05f, 1.0f);
+    auto scaleSeq = Sequence::create(scale1, scale2, nullptr);
+    getChildByTag(Tag::IMG_TITLE)->runAction(scaleSeq->clone());
+    
+    // 버튼
+    getChildByTag(Tag::BTN_VIDEO)->runAction(scaleSeq->clone());
+    
+    // 콜백
+    auto delay = DelayTime::create(ENTER_DURATION);
+    auto callFunc = CallFunc::create([=]() {
+        
+        this->onEnterActionFinished();
+        SB_SAFE_PERFORM_LISTENER(this, onFinished);
+    });
+    runAction(Sequence::create(delay, callFunc, nullptr));
+}
+
+/**
+ * 퇴장 연출
+ */
+void ContinuePopup::runExitAction(SBCallback onFinished) {
+    
+    BasePopup::runExitAction(onFinished);
+    
+    // 배경
+    runBackgroundFadeOutAction(nullptr, 0.15f);
+    
+    // 타이틀
+    auto scale = ScaleTo::create(0.15f, 0);
+    getChildByTag(Tag::IMG_TITLE)->runAction(scale->clone());
+    
+    // 버튼
+    getChildByTag(Tag::BTN_VIDEO)->runAction(scale->clone());
+    
+    // 콜백
+    SBDirector::getInstance()->setScreenTouchLocked(true);
+    
+    auto delay = DelayTime::create(EXIT_DURATION);
+    auto callFunc = CallFunc::create([=]() {
+        
+        SBDirector::getInstance()->setScreenTouchLocked(false);
+        this->onExitActionFinished();
+        SB_SAFE_PERFORM_LISTENER(this, onFinished);
+    });
+    runAction(Sequence::create(delay, callFunc, nullptr));
+}
+
+/**
+ * 등장 연출 완료
+ */
+void ContinuePopup::onEnterActionFinished() {
+    
+    BasePopup::onEnterActionFinished();
+    
+    countdown();
 }
--- Classes/game/ui/ContinuePopup.hpp
+++ Classes/game/ui/ContinuePopup.hpp
@@ -14,7 +14,15 @@
 #include "ui/CocosGUI.h"
 #include "superbomb.h"
 
-class ContinuePopup : public SBBasePopup {
+#include "BasePopup.hpp"
+
+class ContinuePopup : public BasePopup {
+private:
+    enum Tag {
+        IMG_TITLE = 100,
+        BTN_VIDEO,
+    };
+    
 public:
     CREATE_FUNC(ContinuePopup);
     ~ContinuePopup();
@@ -25,23 +33,29 @@ private:
     bool init() override;
     void onExit() override;
     
-    void initBg();
-    void initMenu();
-    
-    void close();
+    void initBackgroundView() override;
+    void initContentView() override;
     
     void countdown();
     void timeOut();
     
+    void updateCountdownImage();
+    
+public:
+    void runEnterAction(SBCallback onFinished = nullptr) override;
+    void runExitAction(SBCallback onFinished = nullptr) override;
+    
+    void onEnterActionFinished() override;
+    
 private:
-    CC_SYNTHESIZE(SBCallback, onClosedListener, OnClosedListener);
     CC_SYNTHESIZE(SBCallback, onVideoListener, OnVideoListener);
     CC_SYNTHESIZE(SBCallback, onTimeOutListener, OnTimeOutListener);
     
-    cocos2d::Node *contentLayer;
-    
     float elapsed;
+    cocos2d::Sprite *countdownImage;
     cocos2d::Label *countdownLabel;
+    
+    int count;
 };
 
 #endif /* ContinuePopup_hpp */
