cb0d8475555d23db3efe9e227ec6aa765cf12106
seongmin hwang <>
2018-11-04 06:10:44 +0900

    [modified] 라이프 사이클에 따른 플러그인 함수를 super 함수보다 먼저 호출하도록 수정

--- proj.android-studio/app/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android-studio/app/src/org/cocos2dx/cpp/AppActivity.java
@@ -54,25 +54,25 @@ public class AppActivity extends Cocos2dxActivity {
 
     @Override
     protected void onStart() {
-        super.onStart();
         PluginManager.onStart();
+        super.onStart();
     }
     @Override
     protected void onStop() {
-        super.onStop();
         PluginManager.onStop();
+        super.onStop();
     }
 
     @Override
     protected void onResume() {
-        super.onResume();
         PluginManager.onResume();
+        super.onResume();
     }
 
     @Override
     protected void onPause() {
-        super.onPause();
         PluginManager.onPause();
+        super.onPause();
     }
 
     @Override
