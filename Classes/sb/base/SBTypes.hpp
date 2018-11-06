db39c62c59bba54d4f18f5f37229f1ba1ca7ad4d
seongmin hwang <>
2018-11-06 16:44:50 +0900

    [added] IntList 타입 추가

--- Classes/sb/base/SBTypes.hpp
+++ Classes/sb/base/SBTypes.hpp
@@ -22,7 +22,8 @@ typedef long double BigFloat;
 
 NS_SB_END
 
-typedef std::vector<std::string> StringList;
+typedef std::vector<int>            IntList;
+typedef std::vector<std::string>    StringList;
 
 typedef enum {
     BOTTOM = 100,
